6f3c8eeb2656f622fa452261e45cb50b1ee22868
Miguel Ossa <ossa71924@gmail.com>
2025-07-06 03:04:50 +0000

    este es el usuario del admin pero no me deja iniciar sesión haz que a es

--- src/lib/user-data.ts
+++ src/lib/user-data.ts
@@ -8,4 +8,5 @@ export const mockUsers: User[] = [
   { id: '3', email: 'waiter@gustogo.com', password: 'password', name: 'Luigi', role: 'waiter' },
   { id: '4', email: 'customer@gustogo.com', password: 'password', name: 'Anton Ego', role: 'customer' },
   { id: '5', email: 'pending@gustogo.com', password: 'password', name: 'New Waiter', role: 'pending' },
+  { id: '6', email: 'ossa71924@gmail.com', password: '12345', name: 'Admin Ossa', role: 'admin' },
 ];
