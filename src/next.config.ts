07b6cd5176dc463c59945e8f2f0e02628e68f082
Miguel Ossa <ossa71924@gmail.com>
2025-07-07 00:19:14 +0000

    Runtime Error
    Error: Invalid src prop (https://www.recetasnestle.com.ec

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.recetasnestle.com.ec',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
