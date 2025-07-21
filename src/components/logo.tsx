e2cc6f44a0385fe5ef3bc2dbc151ac9a9f3fead7
Miguel Ossa <ossa71924@gmail.com>
2025-07-21 23:54:06 +0000

    que al cambiar el nombre del restaurante se modifique

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,16 @@
+
+'use client';
+
 import React from 'react';
 import { UtensilsCrossed } from 'lucide-react';
+import { usePreferences } from '@/hooks/use-preferences';
 
 export const Logo = ({ className }: { className?: string }) => {
+  const { restaurantName } = usePreferences();
   return (
     <div className={`inline-flex items-center justify-center ${className}`}>
         <UtensilsCrossed className="w-1/3 h-1/3 text-primary" />
-        <span className="font-headline text-4xl ml-2 text-foreground">GustoGo</span>
+        <span className="font-headline text-4xl ml-2 text-foreground">{restaurantName}</span>
     </div>
   );
 };
